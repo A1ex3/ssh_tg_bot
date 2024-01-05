656c75729d5bbf6ad1062570ec74819d2bdc9a71
A0n1x <pakshin.2018@gmail.com>
2024-01-05 17:11:42 +0700

    updated

--- ssh_notification.sh
+++ ssh_notification.sh
@@ -6,7 +6,7 @@ LOGGED_HOST="$(hostname -f)"
 LOGGED_TTY="$(ps -p "$$" -o tname h)"
 LOGGED_IP="$(w -i | grep -w "$LOGGED_TTY" | awk '{print $3}')"
 
-sudo /usr/local/bin/sshtgbot/ssh_tg_bot \
+/usr/local/bin/sshtgbot/ssh_tg_bot \
     -config_path="/usr/local/bin/sshtgbot/config.json" \
     -user="$LOGGED_USER" \
     -host="$LOGGED_HOST" \
